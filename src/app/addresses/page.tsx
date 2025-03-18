6ed4f1d7190232ef2e70d92074c127f969f19383
Ros Pavlov <avgust13@users.noreply.github.com>
2025-03-18 16:23:42 +0800

    make page dynamic on build

--- src/app/addresses/page.tsx
+++ src/app/addresses/page.tsx
@@ -1,19 +1,12 @@
-"use client";
+export const dynamic = "force-dynamic";
 
 import type React from "react";
-import { Client, fetchExchange, Provider } from "urql";
 
 import LandingPage from "@features/address-book/components/LandingPage";
 
-const client = new Client({
-  url: process.env.NEXT_PUBLIC_API_URL || "",
-  exchanges: [fetchExchange],
-});
 
 export default function AddressesPage() {
   return (
-    <Provider value={client}>
       <LandingPage />
-    </Provider>
   );
 }
--- src/features/address-book/components/LandingPage.tsx
+++ src/features/address-book/components/LandingPage.tsx
@@ -2,6 +2,7 @@
 
 import type React from "react";
 import { type FC, useState } from "react";
+import { Client, fetchExchange, Provider } from "urql";
 
 import { Typography, Box } from "@mui/material";
 
@@ -10,6 +11,10 @@ import type { AddressType } from "../types";
 import AddressList from "./AddressList";
 import AddressDetails from "./AddressDetails";
 
+const client = new Client({
+  url: process.env.NEXT_PUBLIC_API_URL || "",
+  exchanges: [fetchExchange],
+});
 
 const LandingPage: FC = () => {
   const [selectedAddress, setSelectedAddress] = useState<AddressType | null>(
@@ -17,29 +22,31 @@ const LandingPage: FC = () => {
   );
 
   return (
-    <Box sx={{ display: "flex" }}>
-      {/* Sidebar with addresses */}
-      <AddressList onSelect={setSelectedAddress} />
+    <Provider value={client}>
+      <Box sx={{ display: "flex" }}>
+        {/* Sidebar with addresses */}
+        <AddressList onSelect={setSelectedAddress} />
 
-      {/* Main Content Area */}
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          mt: "64px", // margin top to account for the AppBar
-          ml: "400px", // margin left to account for the Drawer
-        }}
-      >
-        {selectedAddress ? (
-          <AddressDetails address={selectedAddress} />
-        ) : (
-          <Typography variant="h6">
-            Select an address from the sidebar to view details.
-          </Typography>
-        )}
+        {/* Main Content Area */}
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            p: 3,
+            mt: "64px", // margin top to account for the AppBar
+            ml: "400px", // margin left to account for the Drawer
+          }}
+        >
+          {selectedAddress ? (
+            <AddressDetails address={selectedAddress} />
+          ) : (
+            <Typography variant="h6">
+              Select an address from the sidebar to view details.
+            </Typography>
+          )}
+        </Box>
       </Box>
-    </Box>
+    </Provider>
   );
 };
 
