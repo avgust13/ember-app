0b58e115f0bf3f25b67fe3c152ddfdda73e096a9
Ros Pavlov <avgust13@users.noreply.github.com>
2025-03-18 15:03:53 +0800

    Refactor RootLayout component to simplify Box styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Box sx={{ display: "flex" }}>
+        <Box>
           {/* Top Navigation */}
           <AppBar position="fixed">
             <Toolbar>
