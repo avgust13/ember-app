75415f2c823877c821c944088670f0373ab55b9d
Ros Pavlov <avgust13@users.noreply.github.com>
2025-03-18 14:22:57 +0800

    Add AddressesPage component and AddressType interface for address book feature

---
+++ src/app/addresses/page.tsx
@@ -0,0 +1,9 @@
+import type React from 'react';
+
+import LandingPage from '@/src/features/address-book/components/LandingPage';
+
+export default function AddressesPage() {
+  return (
+    <LandingPage />
+  );
+}
